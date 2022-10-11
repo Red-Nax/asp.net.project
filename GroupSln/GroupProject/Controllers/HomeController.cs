58cc3a2bffa40b37cc7aa8c59d4c19db89eb90eb
TT-Apollo <danialpnielsen@gmail.com>
2022-10-11 17:03:23 +0100

    Creating user works - dp

--- GroupSln/GroupProject/Controllers/HomeController.cs
+++ GroupSln/GroupProject/Controllers/HomeController.cs
@@ -2,31 +2,42 @@
 using ASP_NetProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
-
 namespace ASP_NetProject.Controllers;
-
 
 public class HomeController : Controller
 {
     private IStoreRepository repository;
-    
+
     public HomeController(IStoreRepository repo)
     {
-
         repository = repo;
-
     }
-    
-    
-    public IActionResult Index() => View(repository.Users);
 
+
+    public IActionResult Index() => View(repository.Users);
 
     public IActionResult Login() => View();
 
-    public async Task<IActionResult> Details(long id = 1)
+    [HttpPost]
+    public IActionResult Login(User user)
     {
+        repository.CreateUser(user);
         return View();
-        
     }
 
+
+    public async Task<IActionResult> Details()
+    {
+        return View(repository.Users.ToList());
+    }
+
+    // [HttpGet]
+    // public ViewResult RsvpForm() {
+    //     return View();
+    // }
+    // [HttpPost]
+    // public ViewResult RsvpForm(GuestResponse guestResponse) {
+    //     // TODO: store response from guest
+    //     return View();
+    // }
 }
